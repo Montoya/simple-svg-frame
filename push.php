0db1d3978219d05da442330d5759884ebf5a93e4
Montoya <sirokai@gmail.com>
2024-01-27 23:13:15 -0500

    OK, I guess I need PHP

---
+++ push.php
@@ -0,0 +1,10 @@
+<?php header('Content-Type: text/html'); ?>
+<!DOCTYPE html>
+<html>
+  <head>
+      <meta property="og:title" content="Red Button, pushed" />
+      <meta property='og:image' content="https://homerow.club/red-button/assets/fireworks.svg" />
+      <meta property="fc:frame" content="vNext" />
+      <meta property="fc:frame:image" content="https://homerow.club/red-button/assets/fireworks.svg" />
+  </head>
+</html>
